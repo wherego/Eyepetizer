f8921ced3165daea08c27d96c85b0b4aa3f77f3f
jinlong.zhou <jinlong.zhou@leediancn.com>
2016-09-12 15:10:46 +0800

    add api url

--- app/src/main/java/me/racofix/eyes/api/Constant.java
+++ app/src/main/java/me/racofix/eyes/api/Constant.java
@@ -4,5 +4,26 @@ package me.racofix.eyes.api;
  * Created by zjl on 16-9-12.
  */
 public class Constant {
-    static final String API_BASE_URL ="http://baobab.wandoujia.com/api/";
+    static final String API_BASE_URL = "http://baobab.wandoujia.com/api/";
+
+    //视频流API
+    //http://baobab.wandoujia.com/api/v1/feed?num=2
+
+    //视频详情API
+    //http://baobab.wandoujia.com/api/v1/video/2492
+
+    //官网视频最近6个视频API
+    //http://baobab.wandoujia.com/api/v1/video/related/2492/?num=6
+
+    //官网视频详情页面
+    //http://www.wandoujia.com/eyepetizer/detail.html?vid=2492
+
+    //发现更多
+    //http://baobab.wandoujia.com/api/v2/categories?udid=26868b32e808498db32fd51fb422d00175e179df&vc=83
+
+    //热门排行
+    //http://baobab.wandoujia.com/api/v3/ranklist?num=10&strategy=%s&udid=26868b32e808498db32fd51fb422d00175e179df&vc=83
+
+    //发现更多详情接口
+    //http://baobab.wandoujia.com/api/v3/videos?categoryName=%s&strategy=%s&udid=26868b32e808498db32fd51fb422d00175e179df&vc=83
 }
